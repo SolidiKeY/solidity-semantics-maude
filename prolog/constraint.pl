b84ad3623042b03090476e47c7f7b015f7208b6e
Guilherme <guilhermehas@hotmail.com>
2025-02-17 23:12:57 +0100

    fixed constraint program

--- prolog/constraint.pl
+++ prolog/constraint.pl
@@ -1,8 +1,12 @@
 :- use_module(library(chr)).
 
-:- chr_constraint neq/2, vDiff/2.
+:- chr_constraint eq/2, neq/2, vDiff/2.
 
+eq(X,X) <=> true.
 neq(X,X) <=> false.
+neq(X,Y) \ neq(X,Y) <=> true.
+eq(X,Y), neq(X,Y) <=> false.
+eq(X,Y), neq(Y,X) <=> false.
 
 isFId($acc).
 isFVal($age).
@@ -10,11 +14,11 @@ isFVal($age).
 read(mtMem, _, P, 0) :- isFVal(P).
 read(mtMem, Id, P, idC(Id, [P])) :- isFId(P).
 
-read(write(_, Id, S, Val), Id, S, Val).
+read(write(_, Id1, S, Val), Id2, S, Val) :- eq(Id1, Id2).
 
 read(write(Mem, Id1, Sel1, _), Id2, Sel2, X) :-
-    Sel1 \== Sel2;
-    neq(Id1, Id2),
+    (Sel1 \== Sel2;
+    neq(Id1, Id2)),
     read(Mem, Id2, Sel2, X).
 
 read(add(_, Id), Id, P, idC(Id, [P])) :- isFId(P).
@@ -31,5 +35,5 @@ ex(0, X) :-
 ex(1, X) :-
     Mem1 = add(mtMem, idA),
     vDiff(Mem1, idB),
-    Mem2 = write(Mem1, idB, $acc, _),
+    Mem2 = write(Mem1, idB, $acc, 0),
     read(Mem2, idA, $acc, X).
